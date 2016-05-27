9db7dd8a578dcb3bf2cb5a9480c8a5eb0e3e9e31
jialinxie <lin990033@gmail.com>
2016-05-27 09:54:05 +0800

    V1.3 修改触摸屏校准的信息显示方案

--- CODE/CODE STM32_ZIGBEE V1.1_触摸屏测试/USER/ff.d
+++
@@ -1,6 +0,0 @@
-.\ff.o: ..\FATFS\src\ff.c
-.\ff.o: ..\FATFS\src\ff.h
-.\ff.o: ..\FATFS\src\integer.h
-.\ff.o: ..\FATFS\src\ffconf.h
-.\ff.o: ..\FATFS\src\diskio.h
-.\ff.o: F:\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
